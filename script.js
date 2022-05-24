4c6a8d19a2b174ab90cc0bbc4d4eae2b026fec2b
pranav-kural <pranavkural042@gmail.com>
2022-05-23 18:36:30 -0600

    complete logic to add country entered by user

--- script.js
+++ script.js
@@ -3,57 +3,18 @@
 // import data needed to retrieve Alpha-3 code for a country
 import countryCodesData from "./data/countries.json" assert { type: "json" };
 
+// global variables
+const countriesAlreadyInView = [];
+
 // DOM elements
 const countriesContainer = document.querySelector(".countries");
-
-if (navigator?.geolocation) {
-  // get user location
-  navigator.geolocation.getCurrentPosition(addCurrentCountry, (err) =>
-    renderError(err)
-  );
-}
-
-const getCountryAlpha3Code = (country) =>
-  countryCodesData.find((el) => el.name === "Angola")["alpha-3"];
-
-const renderError = (message) =>
-  countriesContainer.insertAdjacentText("beforeend", message);
-
-function addCurrentCountry(userCoords) {
-  // validation
-  if (!userCoords) return alert("Invalid user location!");
-
-  const { latitude } = userCoords.coords;
-  const { longitude } = userCoords.coords;
-
-  getReverseGeocodedLocation({ latitude: latitude, longitude: longitude });
-}
-
-// reverse geocoding the coordinates
-function getReverseGeocodedLocation(coords) {
-  const URL = `https://geocode.xyz/${coords.latitude},${coords.longitude}?geoit=json`;
-  // get country name
-  fetch(URL)
-    .then((response) => {
-      if (!response.ok)
-        throw new Error(`Unable to do reverse geocoding (${response.status})`);
-
-      if (response.status === "403")
-        throw new Error(
-          `Too many requests within a second. Please wait and try later (${response.status})`
-        );
-
-      return response.json();
-    })
-    .then((data) => addCountriesToDOM(data.country.slice(0, 3).toUpperCase()));
-}
+const addCountryInput = document.querySelector("#input__add__country");
+const addCountryButton = document.querySelector(".btn__add__country");
+const deleteAllCountryButton = document.querySelector(".btn__delete__all");
 
 const addCountriesToDOM = (...countries) => {
   // validation
   if (!countries) return alert("Invalid country codes provided!");
-
-  const renderError = (message) =>
-    countriesContainer.insertAdjacentText("beforeend", message);
 
   // get country data
   const fetchCountryData = (countries) => {
@@ -111,5 +72,111 @@ const addCountriesToDOM = (...countries) => {
   fetchCountryData(countries);
 };
 
+const renderError = (message) =>
+  countriesContainer.insertAdjacentText("beforeend", message);
+
+const capitalize = (words) =>
+  words
+    .split(" ")
+    .map((word) => word[0].toUpperCase() + word.slice(1))
+    .join(" ");
+
+const getCountryAlpha3Code = (countryName) => {
+  const capitalizedCountryName = capitalize(countryName);
+  const exactMatch = countryCodesData.find(
+    (el) => el.name === capitalizedCountryName
+  );
+  if (exactMatch) return [exactMatch?.["alpha-3"]];
+  const nearMatches = countryCodesData.filter((el) =>
+    el.name.includes(capitalizedCountryName)
+  );
+  return nearMatches.map((nearMatch) => nearMatch?.["alpha-3"]);
+};
+
+const addCountryToDOM = () => {
+  // validate user input
+  if (!addCountryInput.value) return alert("Enter country name!");
+  // get user input
+  const userInput = addCountryInput.value?.trim();
+  // search for a matching country
+  const countryAlpha3Codes = getCountryAlpha3Code(userInput);
+  console.log("countryAlpha3Codes", countryAlpha3Codes);
+  // if country not found
+  if (countryAlpha3Codes.length === 0)
+    return alert(`Country "${userInput}" not found!`);
+  // remove countries already in view
+  const filteredCountryCodes = countryAlpha3Codes.filter(
+    (country) => !countriesAlreadyInView.includes(country)
+  );
+  // if no countries to add, return
+  if (filteredCountryCodes.length === 0)
+    return alert("Country already in view");
+  // add countries to DOM
+  addCountriesToDOM(...filteredCountryCodes);
+  // update countries in view
+  filteredCountryCodes.forEach((countryCode) =>
+    countriesAlreadyInView.push(countryCode)
+  );
+  console.log("filteredCountryCodes", filteredCountryCodes);
+};
+
+addCountryInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    addCountryToDOM();
+    addCountryInput.value = "";
+  }
+});
+
+addCountryButton.addEventListener("click", () => {
+  addCountryToDOM();
+  addCountryInput.value = "";
+});
+
+deleteAllCountryButton.addEventListener("click", () => {
+  countriesContainer.remove();
+});
+
 // supply list of country codes
-addCountriesToDOM("IND", "ECU", "PRT", "CHN", "PAK");
+// addCountriesToDOM("IND", "ECU", "PRT", "CHN", "PAK");
+
+/* 
+// Uncomment to unable the logic to get user's current location (coordinates)
+// reverse geocode to get the country's name, and add country card to DOM
+
+if (navigator?.geolocation) {
+  // get user location
+  navigator.geolocation.getCurrentPosition(addCurrentCountry, (err) =>
+    renderError(err)
+  );
+}
+
+reverse geocoding the coordinates
+function getReverseGeocodedLocation(coords) {
+  const URL = `https://geocode.xyz/${coords.latitude},${coords.longitude}?geoit=json`;
+  // get country name
+  fetch(URL)
+    .then((response) => {
+      if (!response.ok)
+        throw new Error(`Unable to do reverse geocoding (${response.status})`);
+
+      if (response.status === "403")
+        throw new Error(
+          `Too many requests within a second. Please wait and try later (${response.status})`
+        );
+
+      return response.json();
+    })
+    .then((data) => addCountriesToDOM(data.country.slice(0, 3).toUpperCase()));
+}
+
+function addCurrentCountry(userCoords) {
+  // validation
+  if (!userCoords) return alert("Invalid user location!");
+
+  const { latitude } = userCoords.coords;
+  const { longitude } = userCoords.coords;
+
+  getReverseGeocodedLocation({ latitude: latitude, longitude: longitude });
+}
+
+*/
